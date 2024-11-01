ed4b14b5edde0417c1cbda41354b6231137f0fa9
onura <onur.akmese@viacesi.fr>
2024-11-01 17:57:57 +0100

    Initialized the project

---
+++ csharp-net-forms-improved-racing-game/Form1.cs
@@ -0,0 +1,10 @@
+namespace csharp_net_forms_improved_racing_game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
