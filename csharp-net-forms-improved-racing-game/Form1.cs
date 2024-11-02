3e959eb9ba41e4f7231d43a49ad687dc8126e7d5
onura <onur.akmese@viacesi.fr>
2024-11-02 17:36:31 +0100

    Added the images

--- csharp-net-forms-improved-racing-game/Form1.cs
+++ csharp-net-forms-improved-racing-game/Form1.cs
@@ -6,5 +6,10 @@ namespace csharp_net_forms_improved_racing_game
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
