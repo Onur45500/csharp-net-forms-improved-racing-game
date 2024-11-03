7a1e7745df54eed6695d7a22d56940ea8abc0868
onura <onur.akmese@viacesi.fr>
2024-11-03 21:13:28 +0100

    Made the road mark move from top to bottom

--- csharp-net-forms-improved-racing-game/MainWindow.xaml.cs
+++ csharp-net-forms-improved-racing-game/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace csharp_net_forms_improved_racing_game
@@ -10,6 +12,7 @@ namespace csharp_net_forms_improved_racing_game
     {
 
         DispatcherTimer gameTimer = new DispatcherTimer();
+        int speed = 15;
 
         public MainWindow()
         {
@@ -19,11 +22,41 @@ namespace csharp_net_forms_improved_racing_game
 
             gameTimer.Tick += GameLoop;
             gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+
+            StartGame();
         }
 
         private void GameLoop(object? sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            foreach (var x in MyCanvas.Children.OfType<Rectangle>())
+            {
+                if ((string)x.Tag == "roadMark")
+                {
+                    Canvas.SetTop(x, Canvas.GetTop(x) + speed);
+
+                    if (Canvas.GetTop(x) > 510)
+                    {
+                        Canvas.SetTop(x, -152);
+                    }
+                }
+            }
+        }
+
+        private void OnKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+        private void OnKeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+
+
+        private void StartGame()
+        {
+            speed = 8;
+            gameTimer.Start();
         }
     }
 }
